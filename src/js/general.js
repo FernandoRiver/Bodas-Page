e84399295bfc1dab796d11775b6978ffc2867dd7
FernandoRiver <85407222+FernandoRiver@users.noreply.github.com>
2022-04-14 12:03:33 -0500

    Add files via upload

--- src/js/general.js
+++ src/js/general.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     changeIMG();
     menuBoton();
+    registro();
 });
 
 
@@ -21,7 +22,6 @@ function changeIMG(){
         if(i+1 > 3) i = 1;
         else    i++; 
     },4000);
-    banner.src = `./src/img/scl-3.jpg`;
 }
 
 const menuBoton = () => {
@@ -40,5 +40,26 @@ const menuBoton = () => {
             lista.classList.add("open");
         }
     });
+    document.addEventListener("click", (e)=>{
+        console.log(e.target.id);
+        if((e.target.id != "m-boton" && e.target.id != "m-opcion") && (boton.classList).contains("open")){
+            boton.classList.remove("open");
+            boton.style.backgroundColor = "rgba(255, 255, 255, 0)";
+            lista.classList.remove("open");
+        }
+    });
 }
 
+const registro = () => {
+    const registroDiv = document.getElementById("registro");
+    
+    for(let i = 1; i<=9; i++){
+        const container = document.createElement('DIV');
+        const img = document.createElement('IMG');
+        container.classList.add("registro-card");
+        img.src = `./src/img/reg-${i}.png`;
+
+        container.appendChild(img);
+        registroDiv.appendChild(container);
+    }
+}
